06cba7497e8f36c1512f8b3ef8e59fad203293ad
ajkumar2 <kashajay25@gmail.com>
2024-09-10 05:32:08 +0000

    learn variable in js

---
+++ 02_variable/variable.js
@@ -0,0 +1,8 @@
+const name = "Ajay Kumar"  // define const mean not changeable
+let roll_no = 12
+var student_class = "fivth"
+area = 45  // not used in development purpose ,  define datatype like var, let, const
+let total_area;
+
+console.log(name);
+console.table([name, roll_no, student_class, area, total_area])
